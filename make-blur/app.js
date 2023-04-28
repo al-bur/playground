0ad0949cca580c18ffb8e3984f04ba4f15860c82
sam606 <sam606@naver.com>
2023-04-28 17:26:10 +0900

    feat: 기본 사이트 구현

---
+++ make-blur/app.js
@@ -0,0 +1,44 @@
+const upload = document.getElementById("upload");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+const download = document.getElementById("download");
+const blurSlider = document.getElementById("blur-slider");
+const sliderValue = document.getElementById("slider-value");
+let img;
+
+function applyBlur() {
+  const blurAmount = blurSlider.value;
+  sliderValue.textContent = blurAmount;
+
+  if (img) {
+    ctx.filter = `blur(${blurAmount}px)`;
+    ctx.drawImage(img, 0, 0, img.width, img.height);
+  }
+}
+
+upload.addEventListener("change", (e) => {
+  const file = e.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+    img = new Image();
+    img.src = event.target.result;
+
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      applyBlur();
+    };
+  };
+
+  reader.readAsDataURL(file);
+});
+
+download.addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = "blurred-image.png";
+  link.href = canvas.toDataURL();
+  link.click();
+});
+
+blurSlider.addEventListener("input", applyBlur);
